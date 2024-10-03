96bdad68874c2584b1f3e0c6e1ed15a8b4d0ee90
Aditya Pathak <apathak1997@gmail.com>
2024-10-03 18:28:29 +0530

    port entered dynamically from .env

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,13 @@ import productRoutes from './routes/product.routes.js';
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000
 
 app.use(express.json());  // Middleware that allows us to accept JSON data in req.body
 
 app.use("/api/products", productRoutes);    
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log('Server started at http://localhost:5000')
+    console.log('Server started at http://localhost:' + PORT)
 });
