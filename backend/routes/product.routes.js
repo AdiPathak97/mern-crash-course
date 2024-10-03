c377e01d346696a6bea061234ac12afd367971c1
Aditya Pathak <apathak1997@gmail.com>
2024-10-03 18:22:40 +0530

    formatting

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -3,12 +3,9 @@ import { getProducts, createProduct, updateProduct, deleteProduct } from "../con
 
 const router = express.Router();
 
-export default router;
-
 router.get("/", getProducts);
-
 router.post("/", createProduct);
-
 router.put("/:id", updateProduct);
-
 router.delete("/:id", deleteProduct);
+
+export default router;
