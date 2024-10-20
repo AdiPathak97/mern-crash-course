77106ec9f9f67955f4bb31c7b075cf65735a2980
Aditya Pathak <apathak1997@gmail.com>
2024-10-20 15:02:43 +0530

    default no product home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import { Container, VStack, Text, SimpleGrid } from '@chakra-ui/react'
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <Container maxW={"container.xl"} py={12}>
+      <VStack spacing={8}>
+        <Text
+            fontSize={30}
+            fontWeight={"bold"}
+            textAlign={"center"}
+            bgGradient={"linear(to-r, cyan.400, blue.500)"}
+            bgClip="text"
+        >
+            Current Products 🚀
+        </Text>
+        
+        <Text
+            fontSize={"xl"}
+            fontWeight={"bold"}
+            textAlign={"center"}
+        >
+            No products found 😢
+            <Link to = '/create'>
+              <Text 
+                // as='span' 
+                color={'blue.500'} 
+                _hover={{
+                  textDecoration: 'underline'
+                }}
+              >
+                Create a 🆕 product!   
+              </Text>
+            </Link>
+        </Text>
+
+        <SimpleGrid>
+                
+        </SimpleGrid>
+      </VStack>    
+    </Container>
   )
 }
 
